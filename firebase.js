12f34131ec1b22b45c5e5d991d0f59a4083cdd3e
amrendergill <amrender028@gmail.com>
2022-01-27 15:14:23 +0530

    readme.md

---
+++ firebase.js
@@ -0,0 +1,9 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyAgwXihg29MvBggniQ6qeS3ymcWExToY60",
+  authDomain: "clone-106a1.firebaseapp.com",
+  projectId: "clone-106a1",
+  storageBucket: "clone-106a1.appspot.com",
+  messagingSenderId: "660128556845",
+  appId: "1:660128556845:web:de582da140a3ae8b4695a4",
+};
---
+++ src/components/Banner.js
@@ -0,0 +1,41 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+function Banner() {
+  return (
+    <div className="relative">
+      <div className="absolute w-full h-32 bg-gradient-to-t from-white to-transparent bottom-0 z-20" />
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <div>
+          <img
+            loading="lazy"
+            src="https://m.media-amazon.com/images/I/61eLJJBS-gL._SX3000_.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            loading="lazy"
+            src="https://m.media-amazon.com/images/I/612AqhqwllL._SX3000_.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            loading="lazy"
+            src="https://m.media-amazon.com/images/I/41cvJh2o1dL._SX3000_.jpg"
+            alt=""
+          />
+        </div>
+      </Carousel>
+    </div>
+  );
+}
+
+export default Banner;
